0fe3a5a303414f90cf056947c9148f512ebeb645
Karthik237G <gollapallikarthik03@gmail.com>
2024-08-23 23:36:52 +0530

    Update data collection.py

--- data collection.py
+++ data collection.py
@@ -6,21 +6,21 @@ from google.colab import drive
 drive.mount('/content/drive')
 data=pd.read_csv("dataset.csv")
 
-#data cleaning
-#handle missing values
+#1  data cleaning
+#1.1 handle missing values
 data=data.fillna(data.mean()) #replace NaN values with column mean
 
-#correcting the outliers
+#2 correcting the outliers
 z=np.abs(stats.zscore(data))
 data=data[(z<3).all(axis=1)] #removes rows with outliers based on Z-score
 
-#data transformation 
-#normalize numerical features
+#3 data transformation 
+#3.1 normalize numerical features
 from sklearn.preprocessing import MinMaxScaler
 scaler=MinMaxScaler()
 data[["feature1","feature2"]]=scvalr.fit_transform(data[["feature1","feature2"]])
 
-#encode categorical features
+#3.2 encode categorical features
 from sklearn.preprocessing import LabelEncoder
 lable_encoder=LableEncoder()
 data['category']=label_encoder.fit_transform(data['category'])
