7e61e4725941a135e46b3f8884b29cd27a8d3d80
Karthik237G <gollapallikarthik03@gmail.com>
2024-08-23 23:20:54 +0530

    Update data collection.py

--- data collection.py
+++ data collection.py
@@ -1,2 +1,9 @@
-import pandas as pd 
-import numpy as np
+import pandas as pd #is used for DataFrames and Series
+import numpy as np #multi dimensional arrays
+
+#mounting to drive in colab
+from google.colab import drive
+drive.mount('/content/drive')
+
+data=pd.read_csv("dataset.csv")
+
